96343e283440ea96aa687a8dad040b035dd8003e
Nicolas Hainaux <nh.techn@gmail.com>
2016-07-26 11:57:53 +0200

    Fix: the links to categories.

--- pages/views.py
+++ pages/views.py
@@ -12,13 +12,15 @@ def category(request, **kwargs):
     # todo: check the category does exist
     cat = kwargs.get('category', None)
     # todo: check a result at least is returned
-    active_category = Category.objects.filter(name__exact=cat)[0]
+    active_category = Category.objects.filter(slug__exact=cat)[0]
+    navbar_links = [o.slug for o in Category.objects.all().order_by('order')]
     navbar_entries = [o.name for o in Category.objects.all().order_by('order')]
     leftmenu_entries = [o.name
                         for o in Theme.objects.filter(
                             category_id=active_category.id).order_by('order')]
     return render_to_response('layout.html',
                               {'navbar_entries': navbar_entries,
+                               'navbar_links': navbar_links,
                                'leftmenu_entries': leftmenu_entries,
                                'active': cat,
                                'content': active_category.text,
