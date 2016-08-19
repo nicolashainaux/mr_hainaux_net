a0e7ae8146905ca847a815fef8c2094a2eff6e93
Nicolas Hainaux <nh.techn@gmail.com>
2016-08-19 16:02:32 +0200

    Fixed a display problem; added warning for disabled javascript and for obsolete browsers.

--- static/js/check_js.js
+++ static/js/check_js.js
@@ -2,5 +2,16 @@ window.addEventListener('load', unhideWrapper, false)
 
 function unhideWrapper (event) {
   document.querySelector('#no-js').style.display = 'none'
-  document.querySelector('.wrapper').style.display = 'flex'
+  document.querySelector('.wrapper').style.display = 'block'
+  displayDeprecatedBrowserWarning()
+}
+
+function displayDeprecatedBrowserWarning () {
+  if (typeof Promise !== 'undefined' &&
+      Promise.toString().indexOf('[native code]') !== -1) {
+    var a = document.createElement('a')
+    if (typeof a.download !== 'undefined') {
+      document.querySelector('#deprecated-browser-warning').style.display = 'none'
+    }
+  }
 }
